fc6903cf4160918019e7aa0fe53cca066b5138f8
Yasmineltair <yasmineltair293@gmail.com>
2024-01-31 13:59:28 -0800

    12-binary_tree_leaves.c

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -31,7 +31,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 	if (binary_tree_is_leaf(tree))
-			return (1);
+		return (1);
 	left = binary_tree_leaves(tree->left);
 	right = binary_tree_leaves(tree->right);
 	return (right + left);
